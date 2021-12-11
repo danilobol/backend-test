bcb3547df0c91c4757159a92299c9fc2fbcf2ba7
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-11 18:21:37 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\InstitutionController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserGroupController;
+use App\Http\Controllers\UserRoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +21,69 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/register', [UserController::class,'store']);
+    Route::post('/login', [UserController::class, 'login']);
 });
+
+Route::group([
+    'middleware' => ['api','api.jwt'],
+    'prefix' => 'transaction'
+
+], function ($router) {
+    Route::post('/new', [TransactionController::class,'store']);
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'group'
+
+], function ($router) {
+    Route::post('/create', [GroupController::class,'store'])->middleware('api.jwt');
+    Route::get('/show', [GroupController::class,'show']);
+    Route::get('/my-group-show', [GroupController::class,'showOwnerGroup'])->middleware('api.jwt');
+
+    Route::group([
+        'middleware' => 'api.jwt',
+        'prefix' => 'user'
+    ], function ($router) {
+        Route::post('/add', [UserGroupController::class, 'store']);
+        Route::delete('/delete', [UserGroupController::class, 'delete']);
+        Route::get('/show', [UserGroupController::class, 'index']);
+    });
+
+});
+
+Route::group([
+    'middleware' => ['api','admin.role'],
+    'prefix' => 'admin'
+], function ($router) {
+    Route::group([
+        'prefix' => 'user-role'
+    ], function ($router) {
+        Route::post('/add', [UserRoleController::class, 'addUserRole']);
+        Route::delete('/remove', [UserRoleController::class, 'removeUserRole']);
+        Route::get('/list', [UserRoleController::class, 'getUserRoles']);
+    });
+});
+
+Route::group([
+    'prefix' => 'institution'
+], function ($router) {
+    Route::post('/create', [InstitutionController::class, 'store'])->middleware('admin.role');
+    Route::get('/show/{id}', [InstitutionController::class, 'show']);
+    Route::get('/all', [InstitutionController::class, 'index']);
+
+    Route::group([
+        'prefix' => 'product'
+    ], function ($router) {
+        Route::post('/create', [ProductController::class, 'store'])->middleware('admin.role');
+        Route::get('/show/{id}', [ProductController::class, 'show']);
+        Route::get('/all', [ProductController::class, 'index']);
+    });
+});
+
