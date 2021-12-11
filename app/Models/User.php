d6056d1a1eccc0b3655c8c00260f8ea88b7689a8
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-11 18:18:57 -0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -41,4 +43,38 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /**
+     * @var mixed
+     */
+    private $role;
+    /**
+     * @var mixed
+     */
+    private $user_roles;
+
+    public function getRouteKeyName()
+    {
+        return 'hash_code';
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        $roles = UserRole::FindAccessRoles($this->id);
+        return [
+            'user' => [
+                'id' => $this->id,
+                'email' => $this->email,
+                'nickname' => $this->name
+            ],
+            'roles' => $roles
+        ];
+    }
+
 }
