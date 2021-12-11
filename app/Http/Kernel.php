ba8f7392f109726813a44bcc85201eab6f0295b2
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-11 18:18:21 -0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'api.jwt' => \App\Http\Middleware\ApiProtectedRoute::class,
+        'admin.role' => \App\Http\Middleware\VerifyAdminRole::class,
     ];
 }
