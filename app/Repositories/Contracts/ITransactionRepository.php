19a0f9e8aacb230c34a1657005ad3be9b4438d68
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-11 23:07:54 -0300

    Update ITransactionRepository.php

--- app/Repositories/Contracts/ITransactionRepository.php
+++ app/Repositories/Contracts/ITransactionRepository.php
@@ -8,7 +8,15 @@ interface ITransactionRepository {
         float $amount,
         int $user_id,
         int $group_id,
-        int $product_id
+        ? int $product_id
     );
+    public function detailedStatementOfTransactionsPerGroup(
+        int $userId,
+        int $groupId,
+        int $rowPerPage,
+        int $page
+    );
+    public function getFirstInvestmentDatePerGroup(int $userId, int $groupId);
     public function getTotalUserBalancePerGroup(int $userId, int $groupId);
+    public function getLastDateOfInvestedAmountOfProfit(int $userId, int $groupId);
 }
