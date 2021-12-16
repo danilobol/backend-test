e9e604a1c1ae63784c17206da385b1a4a60a35d9
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-16 04:11:52 -0300

    Delete IUserRoleRepository.php

--- app/Repositories/Contracts/IUserRoleRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Contracts;
-
-interface IUserRoleRepository {
-    public function getAllUsersRole();
-    public function createUserRole(int $userId, int $roleId, int $created_by);
-    public function removeUserRole(int $id_user, int $id_role);
-}
