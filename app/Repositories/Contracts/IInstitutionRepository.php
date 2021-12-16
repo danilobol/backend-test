4adffa46a22687cbb4f248a7ddeb0f70d2532720
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-16 04:11:28 -0300

    Delete IInstitutionRepository.php

--- app/Repositories/Contracts/IInstitutionRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Contracts;
-
-interface IInstitutionRepository {
-    public function createInstitution(string $name);
-    public function getInstitution(int $institutionId);
-    public function getAllInstitutions();
-}
