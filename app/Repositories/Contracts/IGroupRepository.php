ef4113b803aac7ebab423c3f76198a2d9b811d67
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-16 04:11:19 -0300

    Delete IGroupRepository.php

--- app/Repositories/Contracts/IGroupRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Contracts;
-
-interface IGroupRepository {
-    public function createNewGroup(string $name, int $institutions_id, int $owner_id, $amount);
-    public function getGroup(int $groupId);
-    public function getOwnerUserGroups(string $userId): \Illuminate\Database\Eloquent\Collection|array;
-}
