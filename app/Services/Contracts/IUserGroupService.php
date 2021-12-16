480c3984299135d0f72c528ba6db5a108e7737c8
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-16 04:13:53 -0300

    Delete UserGroupService and IUserRoleService

--- app/Services/Contracts/IUserGroupService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Services\Contracts;
-
-interface IUserGroupService {
-    public function addUserToGroup(int $userId, int $groupId, string $type);
-    public function removeUserToGroup(int $userId, int $groupId):void;
-    public function getGroupsOfUser(int $userId);
-}
