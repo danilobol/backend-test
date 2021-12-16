e3cabc0abee59094a7d5558b3e92250461bb9e05
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-16 04:10:48 -0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'api.jwt' => \App\Http\Middleware\ApiProtectedRoute::class,
-        'admin.role' => \App\Http\Middleware\VerifyAdminRole::class,
     ];
 }
