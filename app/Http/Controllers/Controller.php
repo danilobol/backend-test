1258746dba93abfd4e3ea5d62e443a5d18e26e9a
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-16 07:24:37 -0300

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,9 +9,12 @@ use Illuminate\Routing\Controller as BaseController;
 
 /**
  * @OA\Server(url="http://localhost:8100"),
+ * @OA\Link (description="Tutorial", ref="Tutorial", operationId="tutorial", link="https://github.com/danilobol/backend-test/blob/development/Tutorial.pdf"),
  * @OA\Info(title="Challenge-Backend", version="0.0.1",
  *
  *     @OA\Contact(email="danilo.britoxd@hotmail.com", name="Danilo Brito"),
+ *     @OA\Contact(name="https://github.com/danilobol/backend-test/blob/development/Tutorial.pdf"),
+ *
  * )
  *
  *
