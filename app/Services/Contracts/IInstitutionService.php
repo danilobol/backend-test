bba2930222d877036b64f8037915b244430b9330
Danilo Brito <danilo.britoxd@hotmail.com>
2021-12-16 04:12:46 -0300

    Delete IInstitutionService.php

--- app/Services/Contracts/IInstitutionService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Services\Contracts;
-
-interface IInstitutionService {
-    public function createInstitution(string $name);
-    public function getInstitution(int $institutionId);
-    public function getAllInstitutions();
-}
